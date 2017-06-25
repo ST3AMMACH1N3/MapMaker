ac6f3e2bd0b156725ef49d0a8e9d8fd1e6f044b3
ST3AMMACH1N3 <Keeps_to_himself@hotmail.com>
2017-06-25 01:52:07 -0400

    Update teleporter.js

--- teleporter.js
+++ teleporter.js
@@ -5,6 +5,7 @@ function Teleporter(x, y, w, h, id) {
 	this.h = h;
 	this.id = id;
 	this.color = map(id, 0, 10, 0, 255);
+	this.canTeleport = true;
 	
 	this.show = function() {
 		push();
@@ -22,6 +23,25 @@ function Teleporter(x, y, w, h, id) {
 			text("(" + this.x + ", " + this.y + ")", this.x - 56, this.y - 16, 120, 120);
 			text(this.w, this.x + this.w / 2 - 12, this.y - 16, 120, 120);
 			text(this.h, this.x + this.w + 6, this.y + this.h / 2 - 12, 120, 120);
+		}
+	}
+	
+	this.checkCollision() {
+		if (this.canTeleport) {
+			for (var i = 0; i < players.length; i++) {
+				if (players[i].x + players[i].w / 2 > this.x && players[i].x - players[i].w / 2 < this.x + this.w) {
+					if (players[i].y + players[i].w / 2 > this.y && players[i].y - players[i].w / 2 < this.y + this.h) {
+						for (var j = 0; j < teleporters.length; j++) {
+							if (teleporters[j].id == this.id && !(teleporters[j] == this)) {
+								teleporters[j].canTeleport = false;
+								players[i].x = teleporters[j].x;
+								players[i].y = teleporters[j].y;
+								players[i].yspeed = 0;
+							}
+						}	
+					}
+				}
+			}
 		}
 	}
 }
