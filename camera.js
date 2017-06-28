9c5dcf9dda32a8563845d4f4130cba287fc1288a
ST3AMMACH1N3 <Keeps_to_himself@hotmail.com>
2017-06-28 15:50:34 -0400

    Add files via upload

--- camera.js
+++ camera.js
@@ -7,21 +7,53 @@ function Camera() {
 	
 	//move the camera corresponding to keys pressed
 	this.update = function() {
-		//A
-		if (keyIsDown(65)) {
-			this.x -= this.speed;
-		}
-		//D
-		if (keyIsDown(68)) {
-			this.x += this.speed;
-		}
-		//W
-		if (keyIsDown(87)) {
-			this.y -= this.speed;
-		}
-		//S
-		if (keyIsDown(83)) {
-			this.y += this.speed;
+		
+		if (testing) {
+			if (players[0].x > this.x + (width * 2) / 3) {
+				if (players[0].x > this.x + width) {
+					this.x = players[0].x - width / 2;
+				} else {
+					this.x += 5;
+				}
+			}
+			if (players[0].x < this.x + width / 3) {
+				if (players[0].x < this.x) {
+					this.x = players[0].x - width / 2;
+				} else {
+					this.x -= 5;
+				}
+			}
+			if (players[0].y > this.y + (height * 7) / 8) {
+				if (players[0].y > this.y + height) {
+					this.y = players[0].y - (height *  7) / 8;
+				} else {
+					this.y += players[0].yspeed + 1;
+				}
+			}
+			if (players[0].y < this.y + height / 3) {
+				if (players[0].y < this.y) {
+					this.y = players[0].y - (height *  7) / 8;
+				} else {
+					this.y -= 5;
+				}
+			}
+		} else {
+			//A
+			if (keyIsDown(65)) {
+				this.x -= this.speed;
+			}
+			//D
+			if (keyIsDown(68)) {
+				this.x += this.speed;
+			}
+			//W
+			if (keyIsDown(87)) {
+				this.y -= this.speed;
+			}
+			//S
+			if (keyIsDown(83)) {
+				this.y += this.speed;
+			}
 		}
 	}
 	
